0fbb867d39de40a9274e84b8d35b7be409ef0d0a
oleksandr.chemerys <oleksandr.chemerys@arixess.com>
2026-06-17 17:08:23 +0300

    add env pub domain

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
+const backendDomain = process.env.NEXT_PUBLIC_BACKEND_DOMAIN || 'cp.haliotis.space';
+
 export default withNextIntl({
   images: {
-	domains: ['cp.haliotis.space'],
-	qualities: [100, 75],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: backendDomain,
+      },
+    ],
+    qualities: [100, 75],
   },
   typescript: {
     ignoreBuildErrors: true,
